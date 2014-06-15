7dd4631a5a95237b2ded0b0257abb30fbeabf8a4
ted <yihsin2010@gmail.com>
2014-06-15 17:52:41 +0800

    aa

---
+++ quickerAbc/quickerAbc/herodiedwwwViewController.h
@@ -0,0 +1,15 @@
+//
+//  herodiedwwwViewController.h
+//  quickerAbc
+//
+//  Created by admin on 14/6/15.
+//  Copyright (c) 2014年 chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface herodiedwwwViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIButton *pushStart;
+
+
+@end
