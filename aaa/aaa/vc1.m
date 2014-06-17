72d91644a4a82d771986984104d0b31b3dcfb034
tony <tony@tonytekiMacBook-Pro.local>
2014-06-17 21:00:51 +0800

    xx

--- aaa/aaa/vc1.m
+++ aaa/aaa/vc1.m
@@ -31,30 +31,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    int a=0;
-    
-    aa=[NSArray arrayWithObjects:@"momo1.jpg",@"momo2.jpg",@"momo3.jpg",@"momo4.jpg",@"momo5.jpg", nil];
-    
-    for (NSString *str in aa) {
-        
-        
-        //        UIImageView *vi=[[UIImageView alloc]initWithFrame:CGRectMake(0+a*70, 150, 50, 50)];
-        //        a++;
-        //        [self.view addSubview:vi];
-        //        [vi setImage:[UIImage imageNamed:str]];
-        btns=[[UIButton alloc]initWithFrame:CGRectMake(0+a*70, 150, 50, 50)];
-        
-        a++;
-        [self.view addSubview:btns];
-        [btns setImage:[UIImage imageNamed:str] forState:UIControlStateNormal];
-        [UIView animateWithDuration:3.0 animations:^{
-            btns.frame=CGRectMake(btns.frame.origin.x, btns.frame.origin.y+200, btns.frame.size.width, btns.frame.size.height);}];
-        
-        
-        
-        
-    }
-    
+    [self a2];
     // Do any additional setup after loading the view.
 }
 
@@ -74,5 +51,54 @@
     // Pass the selected object to the new view controller.
 }
 */
+//
+-(void)a1
+{
+    int a=0;
+    
+    aa=[NSArray arrayWithObjects:@"momo1.jpg",@"momo2.jpg",@"momo3.jpg",@"momo4.jpg",@"momo5.jpg", nil];
+    
+    for (NSString *str in aa) {
+        
+        
+        //        UIImageView *vi=[[UIImageView alloc]initWithFrame:CGRectMake(0+a*70, 150, 50, 50)];
+        //        a++;
+        //        [self.view addSubview:vi];
+        //        [vi setImage:[UIImage imageNamed:str]];
+        btns=[[UIButton alloc]initWithFrame:CGRectMake(0+a*70, 150, 50, 50)];
+        
+        a++;
+        [self.view addSubview:btns];
+        [btns setImage:[UIImage imageNamed:str] forState:UIControlStateNormal];
+        
+        [UIView animateWithDuration:3.0 animations:^{
+            
+            btns.frame=CGRectMake(btns.frame.origin.x, btns.frame.origin.y+200, btns.frame.size.width, btns.frame.size.height);}];
+        NSLog(@"%@",btns);
+        
+        
+        
+    }
+
+}
+-(void)a2
+{
+    int b=arc4random()%5+1;
+    NSLog(@"a=%d",b);
+    UIImage *momo=[UIImage imageNamed:@"momo1.jpg"];
+    UIButton *mybutton=[UIButton buttonWithType:UIButtonTypeCustom];
+    mybutton.frame=CGRectMake(50, 50, 100, 100);
+    [self.view addSubview:mybutton];
+    [mybutton setImage:momo forState:UIControlStateNormal];
+    [mybutton addTarget:self action:@selector(clickButton:) forControlEvents:UIControlEventTouchDown];
+    [UIView animateWithDuration:3.0 animations:^{
+        
+       mybutton.frame=CGRectMake(mybutton.frame.origin.x, mybutton.frame.origin.y+200, mybutton.frame.size.width, mybutton.frame.size.height);}];
+}
+-(void)clickButton:(int) sender
+{
+    self.lsbel.text=@"@@@@";
+    
+}
 
 @end
